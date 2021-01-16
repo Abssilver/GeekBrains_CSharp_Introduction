a30414e3c894fcfaf9735e033430bdc47057c862
Павел Ремизов <remizov@rightway.games>
2021-01-16 21:22:48 +0300

    Практичекое задание к уроку 1

---
+++ HomeWorks/Lesson_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя пользователя");
+            string userInput = Console.ReadLine();
+            DateTime currentDate = DateTime.Now;
+            //Поставить точку останова здесь
+            Console.WriteLine($"Привет, {userInput}, сегодня {currentDate.ToLongDateString()}");
+            Console.WriteLine("Нажмите любую клавишу для завершения программы");
+            Console.ReadKey();
+        }
+    }
+}
