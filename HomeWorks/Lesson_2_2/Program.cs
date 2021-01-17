5e79a7e6fe1a72224d622b9d4eb4085b8c78f9bb
Павел Ремизов <remizov@rightway.games>
2021-01-17 22:29:01 +0300

    Практическое задание 2

---
+++ HomeWorks/Lesson_2_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lesson_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа перевода числового значения в название месяца привествует вас");
+            int monthNumber = default;
+            GetUserInput(ref monthNumber);
+            Console.WriteLine($"Введенное вами значение соответсвует месяцу: {GetMonthName(monthNumber)}");
+        }
+        private static void GetUserInput(ref int monthNumber)
+        {
+            do
+            {
+                Console.WriteLine($"Введите числовое значение месяца. Примеры : значению 1 будет соотвествовать January");
+            } while (!Int32.TryParse(Console.ReadLine(), out monthNumber) || monthNumber <= 0);
+        }
+
+        private static string GetMonthName(int monthNumber)
+        {
+            return ((Month)(monthNumber % 12)).ToString();
+        }
+        
+        [Flags]
+        private enum Month
+        {
+            December,
+            January,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November
+        }
+    }
+}
