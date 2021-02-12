e719656d48b3eee96cc81cc6773e254bb47065ba
Павел Ремизов <remizov@rightway.games>
2021-02-13 00:25:06 +0300

    Урок 8. Практическое задание. Настройки приложения и его версия

---
+++ HomeWorks/Lesson_8/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Lesson_8
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string output = Properties.Settings.Default.Greeting_p1;
+            if (string.IsNullOrEmpty(Properties.Settings.Default.UserName))
+            {
+                GetDataForSettings();
+            }
+            else
+            {
+                output = string.Concat(
+                    output,
+                    Environment.NewLine,
+                    FillTheFieldsOfOutput());
+            }
+            Console.WriteLine(output);
+            Console.WriteLine("Press any key to finish an adventure");
+            Console.ReadKey();
+        }
+        static string FillTheFieldsOfOutput()
+        {
+            string output = Properties.Settings.Default.Greeting_p2;
+            output = output.Replace("{username}", Properties.Settings.Default.UserName)
+                .Replace("{occupation}", Properties.Settings.Default.UserOccupation)
+                .Replace("{age}", Properties.Settings.Default.UserAge.ToString());
+            return output;
+        }
+        static void GetDataForSettings()
+        {
+            string username = GetUserInput(
+                     "Hello my friend, how can I call you?",
+                     "Ahh, for sure, lately I have noticed more and more problems with my memory...",
+                     NameCheck);
+            byte age = Byte.Parse(GetUserInput(
+                "And how long have you been living under this sun?",
+                "I have not seen you for a long time...",
+                AgeCheck));
+            string occupation = GetUserInput(
+                $"And who are you now, {username}?",
+                "It was always difficult for me to understand what they are doing, people of your profession...",
+                OccupationCheck);
+            Properties.Settings.Default.UserName = username;
+            Properties.Settings.Default.UserAge = age;
+            Properties.Settings.Default.UserOccupation = occupation;
+            Properties.Settings.Default.Save();
+        }
+        static bool NameCheck(string username) => username != null && username.Trim().Length > 0;
+        static bool AgeCheck(string age) => Byte.TryParse(age, out _);
+        static bool OccupationCheck(string occupation) => NameCheck(occupation);
+        private static string GetUserInput(string description, string reaction, Func<string, bool> check)
+        {
+            string userInput = null;
+            if (check == null)
+            {
+                throw new ArgumentException("Check function can not be null!");
+            }
+            do
+            {
+                Console.WriteLine($"{description}");
+                userInput = Console.ReadLine();
+            } while (!check.Invoke(userInput));
+            Console.WriteLine($"{reaction}");
+            return userInput;
+        }
+    }
+}
---
+++ HomeWorks/Lesson_8/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Dialogue_with_an_old_friend")]
+[assembly: AssemblyDescription("This is a dialogue with an old friend, enjoy")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Talkin'")]
+[assembly: AssemblyProduct("Fantasy")]
+[assembly: AssemblyCopyright("Copyright ©  2021 NoRights")]
+[assembly: AssemblyTrademark("MachineCo")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("B31BE38A-56F0-4218-B854-1195D4AB458B")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
